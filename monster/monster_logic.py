4fc8dab05e218e84c090ac227d9ab5a0d7f59008
sam <samuelclark907@gmail.com>
2021-02-15 19:49:55 -0900

    game, monster and character logic

---
+++ monster/monster_logic.py
@@ -0,0 +1,30 @@
+import random
+class Monster:
+    def __init__(self,name, strength, vitality, defense, malice):
+        self.strength = strength
+        self.vitality = vitality
+        self.defense = defense
+        self.malice = malice
+        self.id = "m"
+
+    def attack(self):
+        return self.strength
+
+    def defend(self):
+        self.defense * 1.25
+
+    def haymaker(self):
+        return (self.strength + self.malice) - self.vitality
+
+    def behavior(self):
+        if self.vitality > (self.vitality * .3):
+            return self.attack()
+        else:
+            chance = random.choice((self.haymaker(), self.defend()))
+            return chance
+
+
+if __name__ == "__main__":
+    monst = Monster("john", 10, 10, 10, 10)
+    chance = random.choice((monst.haymaker(), monst.defend()))
+    print(chance)
